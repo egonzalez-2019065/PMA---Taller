22e558ea8c36800ab1fdda7783ecf63f013e81d4
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-02-26 17:02:23 -0600

    Ejercicio de Ahorcado

---
+++ Ahorcado/ahorcado.js
@@ -0,0 +1,12 @@
+
+const palabras =  ["Numeros", "Computadora", "Pelota"];
+
+function getPalabraRandom (palabras){
+    return palabras[Math.floor(Math.random() * palabras.length())];
+    
+}
+
+function generarEspacios(){
+    
+}
+console.log(palabras);
